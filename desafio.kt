8d64f232ef5b9a0b34cc40e961bb4e31461d91e3
Bruno Cícero Rodrigues de Souza <brunoprogramadorjava@gmail.com>
2023-04-24 21:05:21 -0300

    Update desafio.kt
    Desafio bootcamp kotlin Dio

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,78 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
+class Usuario(var nome: String, var email: String, var telefone: String) {
+    val inscricoes = mutableListOf<Formacao>()
+    
+    fun inscrever(formacao: Formacao) {
+        if (!inscricoes.contains(formacao)) {
+            inscricoes.add(formacao)
+            formacao.adicionarInscrito(this)
+        }
+    }
+    
+    fun cancelarInscricao(formacao: Formacao) {
+        if (inscricoes.contains(formacao)) {
+            inscricoes.remove(formacao)
+            formacao.removerInscrito(this)
+        }
+    }
+}
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class Modulo(val nome: String, val duracao: Int)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Curso(val nome: String, var modulos: List<Modulo>) {
+    val duracao: Int
+        get() = modulos.sumOf { it.duracao }
+}
 
+data class Formacao(val nome: String, var cursos: List<Curso>, var nivel: Nivel) {
     val inscritos = mutableListOf<Usuario>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    val duracao: Int
+        get() = cursos.sumOf { it.duracao }
+    
+    fun adicionarInscrito(usuario: Usuario) {
+        if (!inscritos.contains(usuario)) {
+            inscritos.add(usuario)
+            usuario.inscrever(this)
+        }
+    }
+    
+    fun removerInscrito(usuario: Usuario) {
+        if (inscritos.contains(usuario)) {
+            inscritos.remove(usuario)
+            usuario.cancelarInscricao(this)
+        }
+    }
+    
+    fun adicionarCurso(curso: Curso) {
+        cursos += curso
+    }
+    
+    fun removerCurso(curso: Curso) {
+        cursos -= curso
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario1 = Usuario("João", "joao@email.com", "")
+    val usuario2 = Usuario("Maria", "maria@email.com", "")
+    
+    val modulo1 = Modulo("Introdução", 60)
+    val modulo2 = Modulo("Fundamentos", 120)
+    
+    val curso1 = Curso("Java Básico", listOf(modulo1, modulo2))
+    
+    val formacao1 = Formacao("Desenvolvimento Java", listOf(curso1), Nivel.INTERMEDIARIO)
+    
+    formacao1.adicionarInscrito(usuario1)
+    formacao1.adicionarInscrito(usuario2)
+    
+    println("Formação ${formacao1.nome} tem ${formacao1.inscritos.size} inscritos: ${formacao1.inscritos.map { it.nome }}")
+    
+    usuario1.cancelarInscricao(formacao1)
+    
+    println("Formação ${formacao1.nome} tem ${formacao1.inscritos.size} inscritos: ${formacao1.inscritos.map { it.nome }}")
 }
